8ec3e9960ae286fd0c79f2018b02c87048d87ff6
hengkicode <hengkycode@gmail.com>
2020-08-15 14:48:24 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const electron = require('electron') 
+// Importing BrowserWindow from Main 
+const BrowserWindow = electron.remote.BrowserWindow; 
+
+var current = document.getElementById('current'); 
+var options = { 
+	silent: false, 
+	printBackground: true, 
+	color: false, 
+	margin: { 
+		marginType: 'printableArea'
+	}, 
+	landscape: false, 
+	pagesPerSheet: 1, 
+	collate: false, 
+	copies: 1, 
+	header: 'Header of the Page', 
+	footer: 'Footer of the Page'
+} 
+
+current.addEventListener('click', (event) => { 
+	let win = BrowserWindow.getFocusedWindow(); 
+	// let win = BrowserWindow.getAllWindows()[0]; 
+
+	win.webContents.print(options, (success, failureReason) => { 
+		if (!success) console.log(failureReason); 
+
+		console.log('Print Initiated'); 
+	}); 
+}); 
